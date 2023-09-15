0a79d88510ab4947169d2e5b6a921adf81248203
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-09-15 09:48:39 +0800

    Implement main menu layout

--- app/src/main/java/com/suhun/draw/MainActivity.java
+++ app/src/main/java/com/suhun/draw/MainActivity.java
@@ -3,6 +3,7 @@ package com.suhun.draw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goSignFun(View view){
+
+    }
+
+    public void goCircleFun(View view){
+
     }
 }
